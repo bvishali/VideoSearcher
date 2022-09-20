0badc4fdcd576c9027810d68131512c0cdb65343
Vishali Bhatnagar <vbhatnagar@microsoft.com>
2022-09-21 02:02:04 +0530

    redid qna integration

--- VideoSearcherAPI/VideoSearcherAPI/Searcher/VttContentSearcher.cs
+++ VideoSearcherAPI/VideoSearcherAPI/Searcher/VttContentSearcher.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Azure;
+using Azure.AI.Language.QuestionAnswering;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VideoSearcherAPI.Model;
 using VideoSearcherAPI.Parser;
 
 namespace VideoSearcherAPI.Searcher
@@ -10,14 +14,16 @@ namespace VideoSearcherAPI.Searcher
     public class VttContentSearcher
     {
 
-        public static string[] Search(string file, string content)
+        public static QnAResponse Search(string file, string content)
         {
+            QnAResponse answers = new QnAResponse();
             string[] frame = new string[20];
             int k=0;
             VttParser parser = new VttParser();
-            //List<string> subs = parser.ConvertVTTToString(File.Open(file, FileMode.Open), System.Text.Encoding.ASCII);
-            List<SubtitleItem> list = parser.ParseStream(File.Open(file, FileMode.Open), System.Text.Encoding.ASCII);
-            
+            var fileStream = File.Open(file, FileMode.Open);
+            List<string> subs = parser.ConvertVTTToString(fileStream, System.Text.Encoding.ASCII);
+            List<SubtitleItem> list = parser.ParseStream(fileStream, System.Text.Encoding.ASCII);
+            fileStream.Close();
             /*search through sliding window*/
             int windowSize = 0;
             int start=0;
@@ -49,7 +55,47 @@ namespace VideoSearcherAPI.Searcher
                  }
             }
             //Console.WriteLine(subs[1]);
-            return frame;
+
+            fileStream.Close();
+
+            #region QnA Service Connection and retreiving answers
+
+            Uri endpoint = new Uri("https://hackathonlanguage.cognitiveservices.azure.com/");
+            AzureKeyCredential credential = new AzureKeyCredential("b62dfbc45dbf4fae957393a7957d6299");
+            string projectName = "hackathonLanguageProject";
+            string deploymentName = "production";
+            QuestionAnsweringClient client = new QuestionAnsweringClient(endpoint, credential);
+            QuestionAnsweringProject project = new QuestionAnsweringProject(projectName, deploymentName);
+            List<TextDocument> records = new List<TextDocument>();
+            //records.Add(new TextDocument(Guid.NewGuid().ToString(), concatenatedText.ToString().Substring(0, 20400)));	
+            foreach (var elem in subs)
+            {
+                records.Add(new TextDocument(Guid.NewGuid().ToString(), elem));
+            }
+            AnswersFromTextOptions options = new AnswersFromTextOptions("why", records);
+            Response<AnswersFromTextResult> response = client.GetAnswersFromText(options);
+
+            var confidentAnswers = response.Value.Answers.Where(x => x.Confidence > 0.1);
+
+            answers.Question = content;
+            answers.Answers = new List<string>();
+
+            if (confidentAnswers != null && confidentAnswers.Any())
+            {
+                foreach (var ans in confidentAnswers)
+                {
+                    answers.Answers.Add(ans.Answer);
+                }
+            }
+            else
+            {
+                answers.Answers.Add("Couldn't find relevant answer!");
+            }
+
+
+            #endregion
+
+            return answers;
         }
 
         private static bool Exists(string[] lList, string sCompare)
---
+++ VideoSearcherAPI/VideoSearcherWebAPI/Controllers/QnASearchController.cs
@@ -0,0 +1,38 @@
+﻿using Azure.AI.Language.QuestionAnswering;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using VideoSearcherAPI.Model;
+using VideoSearcherAPI.Searcher;
+
+namespace VideoSearcherWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QnASearchController : ControllerBase
+    {
+        private readonly ILogger<QnASearchController> _logger;
+
+        public QnASearchController(ILogger<QnASearchController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public QnAResponse Get(string fileName, string searchWord = "when is reward paid out?")
+        {
+            try
+            {
+                var filePath = Environment.CurrentDirectory.Replace("/bin", string.Empty).Replace("/obj", string.Empty);
+                if (string.IsNullOrWhiteSpace(fileName) || fileName == "none") fileName = "/Content/FY22 Rewards Demystification.vtt";
+                return VttContentSearcher.Search(filePath + "/" + fileName, searchWord);
+                
+            }
+            catch (Exception e)
+            {
+                return new QnAResponse() { Error = $"{e.Message + e.StackTrace}" };
+            }
+        }
+    }
+}
